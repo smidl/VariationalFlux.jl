9432255bfeb9f79ec97de0199314cc81b9610650
smidl <vasek.smidl@gmail.com>
2020-11-13 22:37:44 +0100

    working prediction

--- src/vadam.jl
+++ src/vadam.jl
@@ -1,28 +1,40 @@
 using LinearAlgebra
 using Distributions
 using Flux
-import Flux: Params
+
 import Flux.Optimise:  batchmemaybe, update!
 
 struct GaussPs
     ps
-    ω
+    ωsq
+end
+
+predict(model, x, qθ::GaussPs; N=100) =begin
+    model_aux = deepcopy(model)
+    psa = Flux.params(model_aux)
+    
+    function sample()
+        for (θ,μ,ωsq) in zip(psa,qθ.ps,qθ.ωsq)
+             θ .= μ .+ randn(size(μ))./ωsq
+        end
+        model_aux(x)
+    end
+    map((n)->sample(),1:N)
 end
 
 
 # working code
-function vtrain!(loss, ps, data, opt; cb = () -> ())
-    ps = Flux.Params(ps)
+function vtrain!(loss, ps, data, opt; cb = () -> (), ω0=1e4)
     ps_mean = deepcopy(ps)
-    ω = deepcopy(ps)
-    map((x)->x.=1e8,ω)
+    ωsq = deepcopy(ps)
+    map((x)->(x.=ω0) , ωsq)
     
     # cb = runall(cb)
     for d in data
         # reparametrization trick
         for i=1:length(ps)
             ps_mean[i].=ps[i]
-            ps[i].=ps[i].+randn(length(ps[i]))./ .√ω[i]
+            ps[i].=ps[i].+randn(size(ps[i]))./ ωsq[i]
         end
         gs = gradient(ps) do
           loss(batchmemaybe(d)...)
@@ -33,11 +45,11 @@ function vtrain!(loss, ps, data, opt; cb = () -> ())
         update!(opt, ps, gs)
 
         for i=1:length(ps)
-            ω[i].=opt.state[ps[i]][2]
+            ωsq[i].=sqrt.(opt.state[ps[i]][2])
         end
         cb()
     end
-    GaussPs(ps,ω)
+    GaussPs(ps,ωsq)
 end
   
 # alternative 1, TODO
--- test/vadam.jl
+++ test/vadam.jl
@@ -1,6 +1,8 @@
 using LinearAlgebra
 using Distributions
 using Base.Iterators
+using Flux
+
 # trivial least squares with one point at [0,0] and other at [1.5,1.5]
 # + some noise around those 
 x = [0.2ones(1,5) ones(1,5)] + 0.01randn(1,10)
@@ -9,21 +11,28 @@ y = [zeros(5);1.5*ones(5)] + 0.01randn(10)
 X=[x; x.^2];
 # y = X*p
 
-# least squares with Student-t prior (marginalized ARD)
+model=Dense(2,1)
+
+# least squares with Spike&Slab  prior 
 σ1=100;
 σ2=1.0/10;
 ssl(x) = log(0.9*exp(-0.5*(x/σ1)^2)/σ1+0.1*exp(-0.5*(x/σ2)^2)/σ2)
-logSS(p)=ssl(p[1])+ssl(p[2]);
-logp(p)=-norm(y-(p'*X)[:]).^2 +sum(logSS(p))
+ssl(x::AbstractArray)=mapreduce(ssl,+,x)
 
-using Flux
-include("../src/vadam.jl")
+loss(X)=sum((y-model(X)[:]).^2) - mapreduce(ssl,+,Flux.params(model))
+
+
+include("src/vadam.jl")
 #
 
-μ = [1.0,1.0] 
-ps = Flux.Params([μ])
+ps = Flux.params(model)
 opt = ADAM(0.1)
-data = repeated((  ), 1000)
+data = repeated((X,  ), 10000)
 
-loss()=-logp(μ)
-vtrain!(loss,ps,data,opt)
+
+qμ = vtrain!(loss,ps,data,opt)
+xp = (-1:0.1:3)'
+Xp=[xp; xp.^2];
+yp = predict(model, Xp, qμ)
+ypp=vcat(yp...)
+plot(xp',ypp')
